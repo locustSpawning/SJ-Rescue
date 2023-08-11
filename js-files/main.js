844cc092d26a137dee6f730253973ab12ddaf07b
chels <chelsleonhardt@gmail.com>
2023-08-10 20:52:23 -0400

    started calendar - html css started js - baby chickens came so had to stop!

--- js-files/main.js
+++ js-files/main.js
@@ -1,1 +1,126 @@
 console.log('🐢');
+
+// LEAP YEAR //
+
+const isLeapYear = (year) => {
+    return (
+        (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+        (year % 100 === 0 && year % 400 === 0)
+    );
+};
+const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+};
+
+//CALENDAR CREATION //
+
+let calendar = document.querySelector('.calendar');
+
+const month_names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+let month_picker = document.querySelector('#month-picker');
+const dayTextFormat = document.querySelector('.day-text-format');
+const timeFormat = document.querySelector('.time-format');
+const dateFormat = document.querySelector('.date-format');
+
+month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+    dayTextFormat.classList.remove('showtime');
+    dayTextFormat.classList.add('hidetime');
+    timeFormat.classList.remove('showtime');
+    timeFormat.classList.add('hideTime');
+    dateFormat.classList.remove('showtime');
+    dateFormat.classList.add('hideTime');
+};
+
+const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+        31, //Jan
+        getFebDays(year), //Feb
+        31, //March
+        30, //April
+        31, //May
+        30, //June
+        31, //July
+        31, //Aug
+        30, //Sept
+        31, //Oct
+        30, //Nov
+        31, //Dec
+    ];
+
+    let currentDate = new Date();
+    month_picker.innerHTML = month_names[month];
+    calendar_header_year.innerHTML = year;
+    let first_day = new Date(year, month);
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div');
+        if (i >= first_day.getDay()) {
+            day.innerHTML = i - first_day.getDay() + 1;
+            if (
+                i - first_day.getDay() + 1 === currentDate.getDate() &&
+                year === currentDate.getFullYear() &&
+                month === currentDate.getMonth()
+            ) {
+                day.classList.add('current-date');
+            }
+        }
+        calendar_days.appendChild(day);
+    }
+};
+
+let month_list = calendar.querySelector('.month-list');
+month_names.forEach((e, index) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+    month_list.append(month);
+    month.onclick = () => {
+        currentMonth.value = index;
+        generateCalendar(currentMonth.value, currentYear.value);
+        month_list.classList.replace('show', 'hide');
+        dayTextFormat.classList.remove('hideTime');
+        dayTextFormat.classList.add('showtime');
+        timeFormat.classList.remove('hideTime');
+        timeFormat.classList.add('showtime');
+        dateFormat.classList.remove('hideTime');
+        dateFormat.classList.add('showtime');
+    };
+});
+
+(function () {
+    month_list.classList.add('hideonce');
+})();
+document.querySelector('#pre-year').onclick = () => {
+    --currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+};
+document.querySelector('#next-year').onclick = () => {
+    ++currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+};
+
+let currentDate = new Date();
+let currentMonth = { value: currentDate.getMonth() };
+let currentYear = { value: currentDate.getFullYear() };
+generateCalendar(currentMonth.value, currentYear.value);
+
+//left off at 22:56
