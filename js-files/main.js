55294596c59354f2ea3de72dc3739c8088cdc64a
chels <chelsleonhardt@gmail.com>
2023-08-23 11:21:52 -0400

    to top button functional

--- js-files/main.js
+++ js-files/main.js
@@ -1,1 +1,11 @@
 console.log('🐢');
+
+const toTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add('active');
+    } else {
+        toTop.classList.remove('active');
+    }
+});
