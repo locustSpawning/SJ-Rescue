8113060748314f4c4f40d786816399ba85b8c4b6
chels <chelsleonhardt@gmail.com>
2023-10-08 12:38:07 -0400

    Fixed file paths

--- js-files/animal-profile.js
+++ js-files/animal-profile.js
@@ -95,17 +95,17 @@ var favIconAnimal = document.getElementById('blank-heart-animal');
 var favIconSpecies = document.getElementById('blank-heart-species');
 
 function ToggleHeart() {
-    if (favIconAnimal.src.match('../images/heart.png')) {
-        favIconAnimal.src = '../images/heart (1).png';
+    if (favIconAnimal.src.match('images/heart.png')) {
+        favIconAnimal.src = 'images/heart (1).png';
     } else {
-        favIconAnimal.src = '../images/heart.png';
+        favIconAnimal.src = 'images/heart.png';
     }
 }
 
 function ToggleHeart2() {
-    if (favIconSpecies.src.match('../images/heart.png')) {
-        favIconSpecies.src = '../images/heart (1).png';
+    if (favIconSpecies.src.match('images/heart.png')) {
+        favIconSpecies.src = 'images/heart (1).png';
     } else {
-        favIconSpecies.src = '../images/heart.png';
+        favIconSpecies.src = 'images/heart.png';
     }
 }
